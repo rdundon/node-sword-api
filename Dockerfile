37ae911c63589d96c96fda66781a48a177d8fdc5
Robert Dundon <rmdundon@gmail.com>
2024-05-30 01:17:51 -0400

    Threw examples together to get idea how things work, added debug for VS Code

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,6 @@ COPY package*.json ./
 RUN apt-get update && apt-get install --assume-yes build-essential nodejs npm libcurl4-gnutls-dev zlib1g-dev pkg-config cmake subversion
 RUN npm ci
 COPY *.js ./
-CMD npm start 
+CMD npm start --inspect=0.0.0.0:9229
 EXPOSE 3000
+EXPOSE 9229
--- index.js
+++ index.js
@@ -1,13 +1,54 @@
-// const sword = require('node-sword-interface');
+const NodeSwordInterface = require('node-sword-interface');
+const sword = new NodeSwordInterface();
 
 const express = require('express');
 const app = express();
 const port = 3000;
 
+// Example from README: https://github.com/ezra-bible-app/node-sword-interface?tab=readme-ov-file#example-installing-the-kjv-module
+async function installKJV() {
+    console.log("Updating repository configuration ...");
+    await sword.updateRepositoryConfig();
+  
+    console.log("Installing King James module");
+    // Install the King James Version (Uses the internet connection to download and install the module)
+    await sword.installModule('KJV');
+  }
+  installKJV().then(() => {
+    console.log("Installation of KJV successfully completed!");
+    console.log(printKjvInfo());
+  });
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    let response =  printMatthew();
+  res.send(`Hello World! <br /> ${response}`);
+})    
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+
+
+function printKjvInfo() {
+    // Print some module information
+    var kjv = sword.getLocalModule('KJV');
+    return kjv.description + ' ' + kjv.about;
+  }
+  
+  function printMatthew() {
+    var text = '';
+    // Get the verses of the Gospel of Matthew
+    var verses = sword.getBookText('KJV', 'Mat');
+  
+    // Do something with the verses
+    for (var i = 0; i < verses.length; i++) {
+      var currentVerse = verses[i];
+      var verseReference = currentVerse.chapter + ':' + currentVerse.verseNr;
+      console.log('printMatthew', verseReference + ' '  + currentVerse.content)
+      text = text + verseReference + ' '  + currentVerse.content;
+    }
+    return text;
+  }
+  
